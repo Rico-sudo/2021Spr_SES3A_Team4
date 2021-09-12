95255fbae3772823c0f52af9acb6de3483007b44
Rachana Bhat <53508496+Rachanabhat601@users.noreply.github.com>
2021-09-12 19:35:20 +1000

    RachanaBhatSearchBarFunctionality
    SearchBar Functionality with more modifications

--- react-native-app/components/elements/SearchBar.js
+++ react-native-app/components/elements/SearchBar.js
@@ -1,6 +1,64 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, Component } from 'react';
+import {  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  FlatList,
+  TouchableOpacity,
+  TextInput, Image } from 'react-native';
+  
 
+export default function SearchBar({value, updateSearch, style}){
+  const [query, setQuery] =  useState();
+  const [error, setError] = useState()
+   return (
+      <View style={styles.searchBar}>
+        <View style={styles.searchContainer}>
+         <View style={styles.vwSearch}>
+           <Image
+             style={styles.icSearch}
+             source={require('')} />
+         </View>
+         <TextInput
+                    value={query}
+                    placeholder="Search"
+                    style={styles.textInput}
+                    onChangeText={(text) => {
+                        var letters = /^$|^[a-zA-Z._\b ]+$/;
+                        if (text.length > 12)
+                            setError("Query too long.")
+                        else if (text.match(letters)) {
+                            setQuery(text)
+                            updateSearch(text)
+                            if (error)
+                                setError(false)
+                        }
+                        else setError("Please only enter alphabets")
+                    }}
+                />
+                {
+                    query ?
+                        <TouchableOpacity
+                            onPress={() => setQuery('')}
+                            style={styles.vwClear}>
+                            <Image
+                                style={styles.icClear}
+                                source={require('')} />
+                        </TouchableOpacity>
+                        : <View style={styles.vwClear} />
+                }
+        </View>
+        {
+                error &&
+                <Text style={styles.txtError}>
+                    {error}
+                </Text>
+            }
+      </View>
+    )
+}
 class SearchBar extends Component {
   constructor(props) {
     super(props);
@@ -8,27 +66,64 @@ class SearchBar extends Component {
     };
   }
 
+
   render() {
-    return (
-      <View style={styles.searchBar}>
-        <Text> SearchBar </Text>
-      </View>
-    );
+    //return (
+    //  <View style={styles.searchBar}>
+    //    <Text> SearchBar </Text>
+    //  </View>
+    //);
   }
 }
+export default SearchBar;
 
 const styles = StyleSheet.create({
     searchBar: {
-        width: 363,
-        height: 41,
-        backgroundColor: '#FEFEFE',
-        borderRadius: 20,
-        alignContent: 'center',
-        justifyContent: 'center',
-        shadowColor: '#37373737',
-        shadowOffset: { width: 0, height: 3, },
-        shadowOpacity: 1,
-        shadowRadius: 5,
+      height: 80,
+      alignItems: 'center',
+        //width: 363,
+        //height: 41,
+        //backgroundColor: '#FEFEFE',
+        //borderRadius: 20,
+        //alignContent: 'center',
+        //justifyContent: 'center',
+        //shadowColor: '#37373737',
+        //shadowOffset: { width: 0, height: 3, },
+        //shadowOpacity: 1,
+        //shadowRadius: 5,
+    },
+    searchContainer:
+    {
+        backgroundColor: 'white',
+        width: '90%',
+        height: 40,
+        flexDirection: 'row'
+
+    },
+    txtError: {
+      marginTop: '2%',
+      width: '89%',
+      color: 'white',
+
+    },
+    vwClear: {
+      flex: 0.2,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    textInput: {
+      // backgroundColor: 'green',
+      flex: 1,
+    },
+    vwSearch: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // width: 40,
+    // backgroundColor: 'red'
+    },
+    icSearch: {
+    height: 18, width: 18
     },
 });
 
--- react-native-app/components/pages/Search.js
+++ react-native-app/components/pages/Search.js
@@ -1,24 +1,65 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, Component } from 'react';
+import { SafeAreaView, StyleSheet,
+View, Text, ScrollView, Image,
+StatusBar, FlatList, TouchableOpacity, 
+TextInput } from 'react-native';
+
 import InfoCard from '../elements/InfoCard';
 import SearchBar from '../elements/SearchBar';
+import { MaterialIcons} from '@expo/vector-icons';
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+export default function Search(){
+  const [value, setValue] = useState()
+  function updateSearch(value) {
+      //do your search logic or anything
+      console.log(value)
   }
+  return(
+  <View style={styles.container}>
+          <View style={{height: '20%',backgroundColor: "#F2D88F", borderRadius: 10, }}>
+        
+        <Image
+          style={{ marginTop: '15%', marginLeft: '5%' }}
+          source={require('')} />
+            <SearchBar
+                value={value}
+                updateSearch={updateSearch}
+                    style={{ marginTop: '8%' }}
+                />
+            </View>
 
-  render() {
-    return (
-      <View>
-        <Text>SEARCH PAGE</Text>
-        <SearchBar />
-        <InfoCard /><InfoCard /><InfoCard /><InfoCard />
-      </View>
-    );
-  }
+        </View>
+    )
 }
+ 
 
-export default Search;
+//class Search extends Component {
+  //constructor(props) {
+   // super(props);
+    //this.state = {
+   // };
+ // }
+
+  
+
+  render();
+  {
+   // return (
+      
+     // <view>  
+       // <InfoCard /><InfoCard /><InfoCard /><InfoCard />
+      
+
+      //</view>
+    //)  
+  };
+
+//
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+  },
+}); 
+
+//export default Search;
