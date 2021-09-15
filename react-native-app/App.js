9deecaca409a0f1a13b6f738b37d8fd8beba30a2
shresthaprince <shresthaprince@hotmail.com>
2021-09-16 01:15:29 +1000

    Load snake detection model locally in app

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './components/Main';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Main from "./components/Main";
+import { SnakeDetectorModelContextProvider } from "./context/SnakeDetectorModelContext";
 
 export default function App() {
   return (
-    <Main />
+    <SnakeDetectorModelContextProvider>
+      <Main />
+    </SnakeDetectorModelContextProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ react-native-app/context/SnakeDetectorModelContext.js
@@ -0,0 +1,50 @@
+import React, { createContext, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import { bundleResourceIO } from "@tensorflow/tfjs-react-native";
+
+const SnakeDetectorModelContext = createContext({
+  snakeDetector: null,
+});
+
+export const SnakeDetectorModelContextProvider = ({ children }) => {
+  const [snakeDetector, setSnakeDetector] = useState(null);
+  useEffect(() => {
+    async function loadModel() {
+      await tf.ready();
+      console.log("Loading snake detection model");
+
+      const modelJson = await require("../assets/model/model.json");
+      const modelWeight = await require("../assets/model/group1-shard.bin");
+      const snakeDetectors = await tf.loadLayersModel(
+        bundleResourceIO(modelJson, modelWeight)
+      );
+
+      setSnakeDetector(snakeDetectors);
+      console.log("Model Loaded");
+    }
+    loadModel();
+  }, []);
+
+  const snakeDetectorModelContext = {
+    snakeDetector,
+  };
+
+  return (
+    <SnakeDetectorModelContext.Provider value={snakeDetectorModelContext}>
+      {children}
+    </SnakeDetectorModelContext.Provider>
+  );
+};
+
+export const useSnakeDetectorModel = () => {
+  try {
+    const context = useContext(SnakeDetectorModelContext);
+    if (context === undefined) {
+      throw new Error("Missing Provider");
+    }
+    return context;
+  } catch (err) {
+    console.log("Load Model Context error.", err.message);
+    return {};
+  }
+};
