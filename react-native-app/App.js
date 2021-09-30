9dfb83cfc3685f72565bdc8613c52db30f45f3a5
Rachana Bhat <53508496+Rachanabhat601@users.noreply.github.com>
2021-09-30 13:16:43 +1000

    RachanaSidemenufunctcommit
    Side menu functionality commit for 30sept

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './components/Main';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 
+import {DrawerContent} from './menu/DrawerContent';
 export default function App() {
   return (
     <Main />
   );
 }
-
+const Drawer = createDrawerNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} /> }>
+        <Drawer.Screen name="Home" component = {MainTabScreen}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
