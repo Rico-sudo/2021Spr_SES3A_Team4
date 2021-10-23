8647b5b2d37eb711e555e5a8b513588d0880b355
shresthaprince <shresthaprince@hotmail.com>
2021-10-23 16:40:30 +1100

    Set up snake data history local storage

--- react-native-app/context/SnakeDetectorModelContext.js
+++ react-native-app/context/SnakeDetectorModelContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 import * as tf from "@tensorflow/tfjs";
 import { bundleResourceIO } from "@tensorflow/tfjs-react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SnakeDetectorModelContext = createContext({
   snakeDetector: null,
@@ -8,8 +9,11 @@ const SnakeDetectorModelContext = createContext({
 
 export const snakeClassIds = ["486", "216", "453", "578", "71", "590"];
 
+const SNAKE_DATA_HISTORY_KEY = "@snake_data_history_key";
+
 export const SnakeDetectorModelContextProvider = ({ children }) => {
   const [snakeDetector, setSnakeDetector] = useState(null);
+  const [snakeDataHistory, setSnakeDataHistory] = useState(null);
   useEffect(() => {
     async function loadModel() {
       await tf.ready();
@@ -27,8 +31,38 @@ export const SnakeDetectorModelContextProvider = ({ children }) => {
     loadModel();
   }, []);
 
+  useEffect(() => {
+    const retrieveHistory = async () => {
+      try {
+        const value = await AsyncStorage.getItem(SNAKE_DATA_HISTORY_KEY);
+        setSnakeDataHistory(value);
+      } catch (e) {
+        console.log("Local Storage Retrieve Error", e.message);
+      }
+    };
+    retrieveHistory();
+  }, []);
+
+  const storeSnakeDataHistory = async (value) => {
+    try {
+      const currentHistory = snakeDataHistory || [];
+      currentHistory.unshift(value);
+
+      const jsonValue = JSON.stringify(currentHistory);
+      await AsyncStorage.setItem(SNAKE_DATA_HISTORY_KEY, jsonValue);
+    } catch (e) {
+      console.log("Local Storage Storage Error", e.message);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Check", snakeDataHistory);
+  }, [snakeDataHistory]);
+
   const snakeDetectorModelContext = {
     snakeDetector,
+    snakeDataHistory,
+    storeSnakeDataHistory,
   };
 
   return (
